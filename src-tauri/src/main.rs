72e6681c1b9ccf9b87600029dce0749fc2af4754
Oratual <lautarodominguez@gmail.com>
2025-06-11 04:02:16 +0200

    🌙 Night auto-commit Wed Jun 11 04:02:16 CEST 2025

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -108,3 +108,5 @@ pub use logger_module::LoggerModule;
 pub use auth_module::{AuthModule, AuthConfig};
 pub use logger_module::LoggerModule;
 pub use auth_module::{AuthModule, AuthConfig};
+pub use logger_module::LoggerModule;
+pub use auth_module::{AuthModule, AuthConfig};
