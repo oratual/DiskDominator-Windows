75315367f45478873ba1de5498442b3bad34d477
Oratual <lautarodominguez@gmail.com>
2025-06-11 03:16:15 +0200

    🔄 CI simulation Wed Jun 11 03:16:15 CEST 2025

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -84,3 +84,5 @@ pub use logger_module::LoggerModule;
 pub use auth_module::{AuthModule, AuthConfig};
 pub use logger_module::LoggerModule;
 pub use auth_module::{AuthModule, AuthConfig};
+pub use logger_module::LoggerModule;
+pub use auth_module::{AuthModule, AuthConfig};
